f9356bea85b3d2710a5ca9390d41e72b78b36d4d
Adarsh Raj Yadav <adarshrajyadav68@gmail.com>
2026-01-27 19:21:24 +0530

    lower_bound problem solved

---
+++ binary_search/lower_bound.cpp
@@ -0,0 +1,23 @@
+
+// TC: O(log N)
+// SC: O(1) 
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        // code here
+        int mini=INT_MAX;
+        int l=0;
+        int r=arr.size()-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(arr[mid]>=target){
+                mini=min(mini,mid);
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return mini==INT_MAX ? arr.size():mini;
+    }
+};
