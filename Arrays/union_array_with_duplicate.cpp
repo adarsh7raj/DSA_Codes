76c66e014314e997e22c3920c3988439454c7dfd
Adarsh Raj Yadav <adarshrajyadav68@gmail.com>
2026-01-27 16:07:30 +0530

    find_single_number problem solved

--- Arrays/union_array_with_duplicate.cpp
+++ Arrays/union_array_with_duplicate.cpp
@@ -19,3 +19,12 @@ class Solution {
         return ans;
     }
 };
+
+// as in this question it is said that the arrays are not sorted and we do not have to return the
+//  union array in sorted order.
+// we can use unordered_set or unordered_map to store the elements of both arrays and then return
+// the elements of the set as a vector.
+
+// If the arrays were sorted and we had to return the union array in sorted order
+// Then we could have used the two pointer approach on two sorted arrays to solve this question
+// in O(N+M) time and O(1) auxilary space.
